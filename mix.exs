49d6fa04e29b1878c9184884fbb7158c688c733a
Viper Coder <njovujames@gmail.com>
2023-11-07 23:28:45 +0200

    first commit

--- mix.exs
+++ mix.exs
@@ -26,11 +26,9 @@ defmodule PineUi.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:phoenix_live_view, "~> 0.18.2"},
+      {:phoenix_live_view, "~> 0.20.1"},
       {:poison, "~> 3.1.0"},
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 
